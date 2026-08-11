aa1b10971061c67ffcfdf644f42036a07bb60f1d
Kohi <EMAIL_GITHUB_CUA_BAN@gmail.com>
2026-08-10 23:36:28 -0500

    dsaa

---
+++ Coutcin.py
@@ -0,0 +1,18 @@
+# print("Hello world")
+
+
+
+#1
+
+name=input("Full name: ")
+Age=int(input("Age: "))
+city=input("City: ")
+gpa=float(input("GPA: "))
+
+print("========== STUDENT ==========")
+print("Name:",name)
+print("Age:",Age)
+print("City:",city)
+print("GPA:",f"{gpa:.2f}")
+print("=============================")
+
