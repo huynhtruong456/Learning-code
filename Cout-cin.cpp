ddfcfb95bd0da76a59042742f0bd94c8c614df83
Kohi <EMAIL_GITHUB_CUA_BAN@gmail.com>
2026-09-05 13:57:20 -0500

    testing

--- Cout-cin.cpp
+++ Cout-cin.cpp
@@ -7,30 +7,32 @@
 
 // }
 
-#1
-#include <iostream>
-#include <iomanip>
-int main()
-{   std::string name,city;
-    int age;
-    float gpa;
-    std::cout<<"Full Name: ";
-    std::getline(std::cin, name );
-    std::cout<<"Age: ";
-    std::cin>>age;
-    std::cin.ignore();
-    std::cout<<"City: ";
-    std::getline(std::cin, city);
-    std::cout<<"Gpa: ";
-    std::cin>>gpa;
+// #1
+// #include <iostream>
+// #include <iomanip>
+// int main()
+// {   std::string name,city;
+//     int age;
+//     float gpa;
+//     std::cout<<"Full Name: ";
+//     std::getline(std::cin, name );
+//     std::cout<<"Age: ";
+//     std::cin>>age;
+//     std::cin.ignore();
+//     std::cout<<"City: ";
+//     std::getline(std::cin, city);
+//     std::cout<<"Gpa: ";
+//     std::cin>>gpa;
     
-    std::cout<<"========== STUDENT ==========\n";
-    std::cout<<"Name: "<<name<<std::endl;
-    std::cout<<"Age: "<<age<<std::endl;
-    std::cout<<"City: "<<city<<std::endl;
-    std::cout<<std::fixed<<std::setprecision(2)<<"Gpa: "<<gpa<<std::endl;
-    std::cout<<"=============================";
-    return 0;
+//     std::cout<<"========== STUDENT ==========\n";
+//     std::cout<<"Name: "<<name<<std::endl;
+//     std::cout<<"Age: "<<age<<std::endl;
+//     std::cout<<"City: "<<city<<std::endl;
+//     std::cout<<std::fixed<<std::setprecision(2)<<"Gpa: "<<gpa<<std::endl;
+//     std::cout<<"=============================";
+//     return 0;
 
 
-}
+// }
+
+
